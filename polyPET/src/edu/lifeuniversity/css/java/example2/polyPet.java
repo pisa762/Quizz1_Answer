29e5a91a54c3f58adb5c5a2454b87387909349e3
pisa piseth <pisethpisa21@gmail.com>
2026-03-05 10:45:42 +0700

    update

--- polyPET/src/edu/lifeuniversity/css/java/example2/polyPet.java
+++ polyPET/src/edu/lifeuniversity/css/java/example2/polyPet.java
@@ -2,13 +2,14 @@ package edu.lifeuniversity.css.java.example2;
 
 public class polyPet {
 	 public static void main(String[] args) {
-
+		 
+           //Create an array of Pet objects
 	        Pet[] pets = new Pet[3];
-
-	        pets[0] = new Pet("Kitty");
+	     // Store different subclass objects inside Pet array
+	        pets[0] = new Pet("Doggo");
 	        pets[1] = new Dog("Buddy", "Golden Retriever");
 	        pets[2] = new Dog("Max", "Bulldog");
-
+	     // Demonstrate runtime polymorphism
 	        for (Pet p : pets) {
 	            p.writeOutput();
 
